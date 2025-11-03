04e493dfb73cc7ed6564ee40da908978e995a957
Lucas Dias <DiasLucP@gmail.com>
2025-11-03 15:18:02 -0500

    Fixed pre-populated user passwords

--- model/user.js
+++ model/user.js
@@ -5,13 +5,13 @@ const users = [
 		id: 1,
 		username: 'alice',
 		// password: password123 (bcrypt hash for 'password123')
-		password: '$2a$10$Q9Qw6Qn6Qn6Qn6Qn6Qn6QeQ9Qw6Qn6Qn6Qn6Qn6Qn6Qn6Qn6Qn6',
+		password: '$2a$10$K61ulLQcumrOm/YEf/MGhuoVIRhjRh2P1sFtuaRrMD9l09gvgBAeW', // <-- replace with real hash
 	},
 	{
 		id: 2,
 		username: 'bob',
 		// password: mysecurepass (bcrypt hash for 'mysecurepass')
-		password: '$2a$10$7Q9Qw6Qn6Qn6Qn6Qn6Qn6QeQ9Qw6Qn6Qn6Qn6Qn6Qn6Qn6Qn6',
+		password: '$2a$10$VnfoAs3NSdg9/AX5eZAk7ukXdDwz/e2x8zlacEZciR3jnyl0LAfWq', // <-- replace with real hash
 	},
 ];
 
